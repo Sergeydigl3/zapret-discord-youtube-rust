857467d3baf7fca85c692b7700b634b70c3c85d3
geomi737 <lesh.yt.cool@gmail.com>
2026-06-11 18:05:21 +0300

    Create admin platform variants

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,5 +1,14 @@
 #[cfg(target_os = "windows")]
-pub mod admin;
+pub mod windows;
+
+#[cfg(target_os = "linux")]
+pub mod linux;
 
 #[cfg(target_os = "windows")]
-pub use admin::ensure_admin;
+pub use windows::ensure_admin;
+
+#[cfg(target_os = "linux")]
+pub use linux::ensure_admin;
+
+#[cfg(not(any(target_os = "windows", target_os = "linux")))]
+pub fn ensure_admin() {}
