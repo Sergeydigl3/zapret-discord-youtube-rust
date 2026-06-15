5f8af58f50d5545d027dc838ef7c9753f0b54d06
geomi737 <lesh.yt.cool@gmail.com>
2026-06-15 14:32:05 +0300

    Remove python locales translation

--- update_i18n_locales.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open("locales/ru.yml", "a", encoding="utf-8") as f:
-    f.write('err_no_strats: "Стратегии не скачаны"\n')
-
-with open("locales/en.yml", "a", encoding="utf-8") as f:
-    f.write('err_no_strats: "Strategies not downloaded"\n')
-
--- update_i18n_service.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open("locales/ru.yml", "a", encoding="utf-8") as f:
-    f.write('menu_srv_restart: "🔄 Перезапустить службу"\n')
-
-with open("locales/en.yml", "a", encoding="utf-8") as f:
-    f.write('menu_srv_restart: "🔄 Restart Service"\n')
-
