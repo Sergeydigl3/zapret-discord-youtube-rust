c542d1d23a8d647f900e8a0f3cc4592bbee28b29
SLYAPA <mamaquepro@gmail.com>
2026-07-09 21:28:57 +1000

    Переделывание работы пресетов доменов и убирание не нужного пресета

--- src/autotune/mod.rs
+++ src/autotune/mod.rs
@@ -35,86 +35,8 @@ impl CheckResult {
     }
 }
 
-pub struct DomainPreset {
-    pub name: &'static str,
-    pub domains: &'static [&'static str],
-}
-
-pub const PRESETS: &[DomainPreset] = &[
-    DomainPreset {
-        name: "Discord",
-        domains: &[
-            "discord.com", "discordapp.com", "discordapp.net",
-            "discord.gg", "discordstatus.com", "discord.media",
-            "discord.gift", "discord.gifts", "discord.new",
-            "discord.co", "discord.store", "discord.status",
-            "discord.design", "discord.dev", "discord.app",
-            "discordcdn.com", "discordmerch.com",
-            "discord-activities.com", "discordactivities.com",
-            "discordpartygames.com", "discordsays.com", "discordsez.com",
-            "cdn.discordapp.com",
-            "discord-attachments-uploads.s3.amazonaws.com",
-            "discord-attachments-uploads-prd.storage.googleapis.com",
-        ],
-    },
-    DomainPreset {
-        name: "YouTube",
-        domains: &[
-            "youtube.com", "ytimg.com", "googlevideo.com", "youtu.be",
-            "ggpht.com", "googleusercontent.com", "withyoutube.com",
-            "yt3.ggpht.com", "yt4.ggpht.com",
-            "yt3.googleusercontent.com",
-            "jnn-pa.googleapis.com",
-            "stable.dl2.discordapp.net",
-            "wide-youtube.l.google.com",
-            "youtube-nocookie.com",
-            "youtube-ui.l.google.com",
-            "youtubeembeddedplayer.googleapis.com",
-            "youtubekids.com",
-            "youtubei.googleapis.com",
-            "yt-video-upload.l.google.com",
-            "ytimg.l.google.com",
-            "play.google.com",
-        ],
-    },
-    DomainPreset {
-        name: "Social",
-        domains: &[
-            "twitter.com", "twimg.com", "reddit.com", "redditmedia.com",
-            "t.me", "telegram.org", "instagram.com", "facebook.com",
-        ],
-    },
-    DomainPreset {
-        name: "All",
-        domains: &[
-            "discord.com", "discordapp.com", "discordapp.net",
-            "discord.gg", "discordstatus.com", "discord.media",
-            "discordcdn.com", "discordmerch.com",
-            "discord-activities.com", "discordactivities.com",
-            "discordpartygames.com", "discordsays.com", "discordsez.com",
-            "cdn.discordapp.com",
-            "discord-attachments-uploads.s3.amazonaws.com",
-            "youtube.com", "ytimg.com", "googlevideo.com", "youtu.be",
-            "ggpht.com", "googleusercontent.com", "withyoutube.com",
-            "yt3.ggpht.com", "yt3.googleusercontent.com",
-            "jnn-pa.googleapis.com",
-            "wide-youtube.l.google.com",
-            "youtube-nocookie.com",
-            "youtube-ui.l.google.com",
-            "youtubekids.com",
-            "youtubei.googleapis.com",
-            "ytimg.l.google.com",
-            "play.google.com",
-            "twitter.com", "twimg.com", "reddit.com", "redditmedia.com",
-            "t.me", "telegram.org", "instagram.com", "facebook.com",
-            "whatsapp.com",
-        ],
-    },
-    DomainPreset {
-        name: "Custom",
-        domains: &[],
-    },
-];
+pub mod presets;
+pub use presets::*;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum BlockCheckType {
