a140ad75923a1fae771fb087be1a7ead0c0b492f
SLYAPA <mamaquepro@gmail.com@example.com>
2026-08-29 11:00:27 +1000

    feat(strategy): добавить кастомные стратегии

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,7 +1,9 @@
 pub mod discovery;
+pub mod embedded;
 pub mod parser;
 
 pub use discovery::get_strategies;
+pub use embedded::ensure_custom_strategies;
 #[allow(unused_imports)]
 pub use parser::ParsedStrategy;
 pub use parser::{parse_bat_file, GameFilterPorts};
